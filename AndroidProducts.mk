1746169eb2ec2d027159b6cb26a778d772473fb7
David Nader <star67389@gmail.com>
2017-12-13 18:34:17 -0500

    initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/aospp_marlin.mk \
+	$(LOCAL_DIR)/aosp_marlin_svelte.mk 
---
+++ aospp_marlin.mk
@@ -0,0 +1,69 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Provide meaningful APN configuration
+PRODUCT_COPY_FILES := device/google/marlin/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+PRODUCT_COPY_FILES += \
+	include/misc/bootanimation/custom.zip:system/media/bootanimation.zip
+
+GAPPS_VARIANT := mini
+PRODUCT_NAME := aospp_marlin
+PRODUCT_DEVICE := marlin
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on msm8996
+PRODUCT_MANUFACTURER := google
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_COPY_FILES += device/google/marlin/fstab.common:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.marlin
+GAPPS_FORCE_PACKAGE_OVERRIDES := true
+GAPPS_EXCLUDED_PACKAGES := Hangouts
+GAPPS_FORCE_MMS_OVERRIDES := true
+GAPPS_FORCE_DIALER_OVERRIDES := true
+
+
+$(call inherit-product, device/google/marlin/device-marlin.mk)
+$(call inherit-product-if-exists, vendor/google_devices/marlin/device-vendor-marlin.mk)
+$(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
+
+PRODUCT_PACKAGES += \
+    PixelLauncher \
+    WallpaperPicker \
+    Chrome \
+    VRServices \
+    Google Daydream \
+    DialerGoogle \
+    PackageInstallerGoogle \
+    StorageManagerManager \
+    DialerFramework \
+    StorageManagerGoogle \
+    ClockGoogle \
+    Messanger \
+    FaceUnlock \
+    FaceDetect \
+    CalSync \
+    CalendarGoogle
+
